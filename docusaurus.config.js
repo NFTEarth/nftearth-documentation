372a02ecab3e8b01f914ac9b809cce964ff7216a
Weston Nelson <dogooddowell@protonmail.com>
2023-01-23 05:41:51 +0000

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/NFTEarth/nftearth-documentation',
+            'https://github.com/NFTEarth',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://NFTEarth/nftearth-documentation',
+            'https://github.com/NFTEarth',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
